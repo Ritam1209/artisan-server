265a6487716468bbf04acf64272b09d928f6f197
Ritam <triparnabhadra76@gmail.com>
2026-07-06 14:04:36 +0530

    fix cors for netlify

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,15 @@ const app = express();
 MIDDLEWARE
 ============================== */
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://artellier1.netlify.app"
+    ],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 
